a1d2176a6aa03f90d9a392d50278a36d5a78c5e9
Héctor Fernández Cascallar <hector.fernandez.cascallar@gmail.com>
2018-12-18 19:29:37 +0100

    add fireball and knight guard animations

--- araragame/src/game/scenes/BootScene.js
+++ araragame/src/game/scenes/BootScene.js
@@ -3,9 +3,7 @@ import sky from '@/game/assets/sky.png'
 import testButton from '@/game/assets/test-button-min.png'
 import helmet from '@/game/assets/helmet.png'
 import dragon from '@/game/assets/dragon.png'
-import fireball from '@/game/assets/fireball.png'
 import shield from '@/game/assets/shield.png'
-import knight from '@/game/assets/knight.png'
 
 
 export default class BootScene extends Scene {
@@ -18,9 +16,7 @@ export default class BootScene extends Scene {
     this.load.image('testButton', testButton)
     this.load.image('helmet', helmet)
     this.load.image('dragon', dragon)
-    this.load.image('fireball', fireball)
     this.load.image('shield', shield)
-    this.load.image('knight', knight)
   }
 
   create () {
