2dd479f3db07d26cd30b4dd671e27f181e66b403
Carlos Goce <carlosgoce@gmail.com>
2018-12-18 10:16:47 +0100

    WiP

--- araragame/src/game/scenes/KnightScene.js
+++ araragame/src/game/scenes/KnightScene.js
@@ -12,6 +12,16 @@ export default class KnightScene extends Scene {
         this.add.image(400, 300, 'sky')
         keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         var helmet = this.add.image(100, 400, 'helmet')
+        
+        // Add particles to helmet
+        let particles = this.add.particles('fireblob');
+        let emitter = particles.createEmitter({
+            speed: 100,
+            scale: { start: 1, end: 0 },
+            blendMode: 'ADD',
+            lifespan: 500
+        })
+        emitter.startFollow(helmet);
 
         tween = this.tweens.add({
             targets: helmet,
