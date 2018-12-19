c9a3a448004831f2a035f29f4ea4861585a96974
Daniel García <dgarcia@initios.com>
2018-12-19 12:29:33 +0100

    Recorder class

--- araragame/src/recorder/GameController.js
+++ araragame/src/recorder/GameController.js
@@ -1,3 +1,15 @@
+import Recorder from './Recorder.js'
 export default class GameController {
+  constructor() {
+    var recorder = new Recorder()
 
+    recorder.startRecording()
+
+    recorder.onTalk = function (spell) {
+      // ... function logic ...
+      console.log(spell)
+    };
+    
+    //recorder.stopRecording()
+  }
 }
