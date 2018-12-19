e894ddb1e48e4e2106b221ef16fe5769b37f8633
Héctor Fernández Cascallar <hector.fernandez.cascallar@gmail.com>
2018-12-19 18:37:17 +0100

    refactor

--- araragame/src/recorder/GameController.js
+++ araragame/src/recorder/GameController.js
@@ -1,15 +1,18 @@
 import Recorder from './Recorder.js'
 export default class GameController {
-  constructor() {
+  scene = null
+  constructor(scene) {
     var recorder = new Recorder()
-
+    this.scene = scene;
     recorder.startRecording()
 
-    recorder.onTalk = function (spell) {
+    recorder.onTalk = (spell) => {
       // ... function logic ...
       console.log(spell)
+      this.scene.action();
+      console.log('acción labzada')
     };
-    
+
     //recorder.stopRecording()
   }
 }
