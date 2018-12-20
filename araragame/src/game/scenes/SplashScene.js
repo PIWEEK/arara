8a3728a4eab00ea7d4d5745b9a1525d039c24c8d
Carlos Goce <carlosgoce@gmail.com>
2018-12-20 18:00:59 +0100

    añade libro, spinner de carga, mensaje personalizado

--- araragame/src/game/scenes/SplashScene.js
+++ araragame/src/game/scenes/SplashScene.js
@@ -1,16 +1,21 @@
 import { Scene } from 'phaser'
 
+import book from '@/game/assets/book.png'
+import background from '@/game/assets/game-background.png'
 
 export default class SplashScene extends Scene {
   constructor () {
     super({ key: 'SplashScene' })
   }
 
+  preload () {
+    this.load.image('background', background);
+    this.load.image('book', book)
+  }
+
   create () {
-    this.add.image(0, 0, 'sky').setOrigin(0, 0)
-    this.add.text(0, 0, 'Presiona enter para empezar..', {
-      color: "#fff"
-    })
+    this.add.image(0, 0, 'background').setOrigin(0, 0)
+    this.add.image(252, 80, 'book').setOrigin(0, 0)
 
     this.input.keyboard.once('keyup_ENTER', function() {
         this.scene.start('MenuScene')
