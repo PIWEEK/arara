fbaac80d897eff27afa4d8eb925a34a7fba754fc
Carlos Goce <carlosgoce@gmail.com>
2018-12-20 18:13:14 +0100

    Añade boton a clickar para pasar de pantalla

--- araragame/src/game/scenes/SplashScene.js
+++ araragame/src/game/scenes/SplashScene.js
@@ -2,6 +2,7 @@ import { Scene } from 'phaser'
 
 import book from '@/game/assets/book.png'
 import background from '@/game/assets/game-background.png'
+import hitboxSprite from '@/game/assets/hitbox.png'
 
 export default class SplashScene extends Scene {
   constructor () {
@@ -11,15 +12,22 @@ export default class SplashScene extends Scene {
   preload () {
     this.load.image('background', background);
     this.load.image('book', book)
+    this.load.image('hitbox', hitboxSprite)  
   }
 
   create () {
     this.add.image(0, 0, 'background').setOrigin(0, 0)
     this.add.image(252, 80, 'book').setOrigin(0, 0)
+    this.hitbox = this.add.sprite(485, 410, 'hitbox').setOrigin(0,0).setScale(1.5).setInteractive();
+    // this.hitbox.setAlpha(0);
 
-    this.input.keyboard.once('keyup_ENTER', function() {
-        this.scene.start('MenuScene')
-    }, this)
+    this.hitbox.on('pointerup', (pointer) => {
+      this.scene.start('KnightScene')
+    });
+
+    this.input.keyboard.once('keyup_ENTER', () => {
+        this.scene.start('KnightScene')
+    });
 
   }
 
