6ca00722eedb2a49f92fa7432d76133d9d99c2c3
Héctor Fernández Cascallar <hector.fernandez.cascallar@gmail.com>
2018-12-21 11:11:17 +0100

    change dragon scene background

--- araragame/src/game/scenes/DragonScene.js
+++ araragame/src/game/scenes/DragonScene.js
@@ -2,7 +2,7 @@ import { Scene } from 'phaser'
 import GameController from '@/recorder/GameController.js'
 
 // assets
-import background from '@/game/assets/background.png'
+import dragonBackground from '@/game/assets/dragon-background.png'
 import backgroundFrame from '@/game/assets/background-frame.png'
 import fireballSprite from '@/game/assets/fireball.png'
 import knightSprite from '@/game/assets/knight-animation.png'
@@ -252,7 +252,7 @@ export default class DragonScene extends Scene {
     }
 
     preload() {
-        this.load.image('background', background);
+        this.load.image('dragonBackground', dragonBackground);
         this.load.image('backgroundFrame', backgroundFrame);
         this.load.image('hitbox', hitboxSprite);
         this.load.spritesheet('fireball', fireballSprite, { frameWidth: 165, frameHeight: 335 });
@@ -268,7 +268,7 @@ export default class DragonScene extends Scene {
         this.controls.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.controls.R = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
 
-        this.add.image(0, 0, 'background').setOrigin(0);
+        this.add.image(0, 0, 'dragonBackground').setOrigin(0);
         this.add.image(0, 0, 'backgroundFrame').setOrigin(0).setDepth(100);
         this.textBox = this.add.text(100, 100, '', { fontSize: '48px', fill: '#000' });
         this.knightController = new KnightController(this);
